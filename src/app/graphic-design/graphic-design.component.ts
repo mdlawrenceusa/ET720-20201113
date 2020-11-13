68dabfb1a6342b876a0dbbe8fc6003d352564706
mdlawrenceusa <mdlawrence@me.com>
2020-11-13 13:34:35 -0500

    2 files modified

--- src/app/graphic-design/graphic-design.component.ts
+++ src/app/graphic-design/graphic-design.component.ts
@@ -10,31 +10,22 @@ import { CartService } from '../cart.service';
   styleUrls: ['./graphic-design.component.css']
 })
 export class GraphicDesignComponent implements OnInit {
-  portfolioItems ;
   _albums: any = [] ;
-  imgHeight = 357;
 
   constructor(
     private cartService: CartService,
     private _lightbox: Lightbox,
-  ) {
-
-
-   }
+  ) { }
 
   ngOnInit() {
-    this.portfolioItems = this.cartService.getPortfolio();
-    
     this.cartService.getPortfolio().subscribe(data =>{
       console.log(data);
       this._albums = data;
     });
-
   }
 
   open(index: number): void {
     // open lightbox
-
     this._lightbox.open(this._albums, index);
   }
  
